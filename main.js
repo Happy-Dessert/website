143c02ceda5961b555cc1a0a3dab8e5c17d92d4f
dialahalkhalili <70059987+dialahalkhalili@users.noreply.github.com>
2020-10-04 14:21:59 +0300

    PROJECT

---
+++ main.js
@@ -0,0 +1,30 @@
+console.log('I am connected');
+
+var index = 0;
+
+function animatedImg() {
+  var slideshow = document.getElementsByClassName("img");
+  for ( var i = 0; i < slideshow.length; i++) {
+    slideshow[i].style.display = "none";  
+  }
+  index++;
+  if (index > slideshow.length)
+  	index = 1  
+  slideshow[index-1].style.display = "block";  
+  setTimeout(animatedImg, 2000); 
+}
+
+animatedImg();
+
+var homeTab=document.getElementById('home');
+var storeTab=document.getElementById('store');
+var cartTab=document.getElementById('cart');
+
+// homeTab.onclick = 
+homeTab.onclick=function homePage(){
+	location.href= "index.html";
+}
+
+storeTab.onclick=function storePage(){
+	location.href= "store.html";
+}
