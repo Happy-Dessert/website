5c14b10380cafa3f62553d1d226b696129456c6f
rahaf83 <rahaf.migdad@hotmail.com>
2020-10-05 07:33:02 +0300

    change

--- checkout.js
+++ checkout.js
@@ -16,25 +16,43 @@ cartTab.onclick=function storePage(){
   location.href= "cart.html";
 }
 
-var total=0;
+
+
+/*function addvalue(number){
+   var result=0;
+  var quantity = document.getElementById('quantity').value;
+  
+result=result+(number*quantity);
+  if(quantity==="")
+   alert('You should specify your required quantity')
+  total= total+result;
+    console.log(total);
+}*/
+ /* var array = [document.getElementById('quantity0').value, document.getElementById('quantity1').value, document.getElementById('quantity2').value,
+
+  document.getElementById('quantity3').value]*/
+
+
 var result=0;
 function addvalue(number){
-	result=result+number;
+  var total=0;
+  result=result+number;
   var quantity = document.getElementById('quantity').value;
   if(quantity==="")
-    alert('You should specify your required quantity')
+  alert('You should specify your required quantity')
   total= total+(result*quantity);
-    console.log(total);
+  console.log(total);
+var amount=document.getElementById('amount').innerHTML=total;
+console.log(amount);
 }
-
-// function addvalue(number){
-//   var total=0;
-//   var result=0;
-//   result=result+number;
-//   var quantity = document.getElementById('quantity').value;
-//   if(quantity==="")
-//     alert('You should specify your required quantity')
-//   total= total+(result*quantity);
-//     console.log(total);
+// var result=0;
+// function addvalue(number)
+// {
+// result=result+number;
+// console.log(result);
 // }
+
+
+
+//$("#amount").text(total);
 
